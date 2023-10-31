cb1c61bd583ba12eb8b6ee0d1665e42bce308565
barnabas <barnascript@gmail.com>
2023-10-31 22:50:42 +0100

    feat(individula hero): adjusted the content

--- src/components/IndividualHero/IndividualHero.jsx
+++ src/components/IndividualHero/IndividualHero.jsx
@@ -2,15 +2,19 @@ import { barcode } from "../../exports/images";
 import { Button } from "../../exports/mini_components";
 import styles from "./IndividualHero.module.scss";
 import Image from "next/image";
+
 const Individual = () => {
   return (
     <div className={` sw ${styles.wrapper}`}>
       <div className={`${styles.container}`}>
         <div className={styles.text_div}>
           <div className={styles.hero_text}>
-            <h1>Send and receive parcels anywhere in Nigeria right from Whatsapp</h1>
+            <h1>
+              Send and receive parcels anywhere in Nigeria right from Whatsapp
+            </h1>
             <p className={`subtitle ${styles.welcome_message}`}>
-              Welcome to Droply, your ultimate solution for streamlined and hassle-free last-mile delivery.
+              Welcome to Droply, your ultimate solution for streamlined and
+              hassle-free last-mile delivery.
             </p>
             <Button
               button_bgColor="var(--palatinate-blue)"
