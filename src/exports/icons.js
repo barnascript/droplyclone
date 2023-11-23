902d71f8daaac735b44762b39c405c79404a49b4
barnabas <barnascript@gmail.com>
2023-11-23 12:40:00 +0100

    move assets to public folder

--- src/exports/icons.js
+++ src/exports/icons.js
@@ -1,6 +1,6 @@
-export { default as arrow_down_light } from "../assets/icons/arrow_down_light.svg";
-export { default as arrow_down } from "../assets/icons/arrow_down.svg";
-export { default as eye } from "../assets/icons/eye.svg";
-export { default as logo } from "../assets/icons/logo.svg";
-export { default as logo_white } from "../assets/icons/logo_white.svg";
-export { default as barcode } from "../assets/icons/barcode.svg";
+export { default as arrow_down_light } from "../../public/assets/icons/arrow_down_light.svg";
+export { default as arrow_down } from "../../public/assets/icons/arrow_down.svg";
+export { default as eye } from "../../public/assets/icons/eye.svg";
+export { default as logo } from "../../public/assets/icons/logo.svg";
+export { default as logo_white } from "../../public/assets/icons/logo_white.svg";
+export { default as barcode } from "../../public/assets/icons/barcode.svg";
