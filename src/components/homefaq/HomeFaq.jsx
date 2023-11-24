823c3cbf3ea2e68f4e0337519d219e849ba2cb7f
barnabas <barnascript@gmail.com>
2023-11-24 08:30:55 +0100

    dynamically fetch home page faqs

--- src/components/homefaq/HomeFaq.jsx
+++ src/components/homefaq/HomeFaq.jsx
@@ -1,15 +1,34 @@
+"use client";
+
 import styles from "./HomeFaq.module.scss";
 import { BigButton, Button, Faq } from "../../exports/mini_components";
+import data from "../../components/b";
+import { useState } from "react";
 
 const HomeFaq = () => {
+  const [faqdata, setFaqData] = useState(data);
+
   return (
     <div className={styles.wrapper}>
       <div className={`sw ${styles.container}`}>
         <h5>Frequently Asked Questions</h5>
 
         <div className={styles.faqs}>
-          <Faq question="Who is Droply?" />
-          <Faq question="Who is Droply?" />
+          {faqdata.map((datum) => (
+            <div className={styles.faq} key={datum.id}>
+              <Faq
+                question={datum.question}
+                title1={datum.answer.title1}
+                title2={datum.answer.title2}
+                pointa={datum.answer.first && datum.answer.first.pointa}
+                pointb={datum.answer.first && datum.answer.first.pointb}
+                pointc={datum.answer.first && datum.answer.first.pointc}
+                pointd={datum.answer.first && datum.answer.first.pointd}
+                pointy={datum.answer.second && datum.answer.second.pointy}
+                pointz={datum.answer.second && datum.answer.second.pointz}
+              />
+            </div>
+          ))}
         </div>
 
         <div className={styles.button}>
